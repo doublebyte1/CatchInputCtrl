2940e1226a2d67f45b7488e47528b6f51d1a6858
doublebyte <jo@.(none)>
2011-06-02 20:05:14 +0200

    New Commit

--- CatchInputCtrl/GeneratedFiles/ui_catchinputfrm.h
+++ CatchInputCtrl/GeneratedFiles/ui_catchinputfrm.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'catchinputfrm.ui'
 **
-** Created: Fri 27. May 09:40:55 2011
-**      by: Qt User Interface Compiler version 4.6.3
+** Created: Sun 29. May 14:45:38 2011
+**      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
